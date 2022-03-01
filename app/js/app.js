daeb5e6d87551b1430b7bc4be3e11ba691dee436
MarcoJul <82112168+MarcoJul@users.noreply.github.com>
2022-03-01 22:35:10 +0100

    style: styled the comment box and basic fetch from data

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,11 @@
 "use strict";
 
 console.log("Begin");
+
+const fetchData = async () => {
+  const response = await fetch("../data.json");
+  const data = await response.json();
+  console.log(data);
+};
+
+fetchData();
