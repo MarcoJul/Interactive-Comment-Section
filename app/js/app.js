4c8cfea400dcc72478be4f93138bba4511431ddc
MarcoJul <82112168+MarcoJul@users.noreply.github.com>
2022-03-03 00:04:57 +0100

    feat: added firebase connection and refactoring

--- app/js/app.js
+++ app/js/app.js
@@ -5,6 +5,64 @@
 const commentSection = document.querySelector(".comments");
 const replySection = document.querySelector(".replies");
 
+//// HTML GROUPS
+const createHtmlComments = (comment) => {
+  let commentsHtml = `<div class="comment-box">
+  <div class="user-box">
+  <img
+  src=${comment.user.image.png}
+  alt="profile-pic-${comment.user.username}"
+  />
+  <p class="username">${comment.user.username}</p>
+  <p class="time">${comment.createdAt}</p>
+  </div>
+  <p class="text">
+  ${comment.content}
+  </p>
+  <div class="action">
+  <div class="vote-box">
+  <button>
+  <img src="images/icon-plus.svg" class="vote-button" />
+  </button>
+  <span>${comment.score}</span>
+  <button><img src="images/icon-minus.svg" /></button>
+  </div>
+  <button><img src="images/icon-reply.svg" />Reply</button>
+  </div>
+  </div>
+  <div class="replies"></div>`;
+  return commentsHtml;
+};
+const createReplyHtml = (reply) => {
+  let replyHtml = `<div class="comment-box">
+  <div class="user-box">
+  <img
+  src=${reply.user.image.png}
+  alt="profile-pic-${reply.user.username}"
+  />
+  <p class="username">${reply.user.username}</p>
+  <p class="time">${reply.createdAt}</p>
+  </div>
+  <p class="text">
+  ${reply.content}
+  </p>
+  <div class="action">
+  <div class="vote-box">
+  <button>
+  <img src="images/icon-plus.svg" class="vote-button" />
+  </button>
+  <span>${reply.score}</span>
+  <button><img src="images/icon-minus.svg" /></button>
+  </div>
+  <button><img src="images/icon-reply.svg" />Reply</button>
+  </div>
+  </div>
+  <div class="replies"></div>`;
+  return replyHtml;
+};
+
+/// FETCH OPERATIONS
+
 const fetchData = async () => {
   try {
     const response = await fetch(
@@ -18,67 +76,23 @@ const fetchData = async () => {
   }
 };
 
+//// OPERATIVE SECTION
+
 const loadComments = async () => {
   const comments = await fetchData();
 
   if (comments) {
     comments.forEach((comment) => {
-      let html = `<div class="comment-box">
-    <div class="user-box">
-    <img
-    src=${comment.user.image.png}
-    alt="profile-pic-${comment.user.username}"
-    />
-    <p class="username">${comment.user.username}</p>
-    <p class="time">${comment.createdAt}</p>
-    </div>
-    <p class="text">
-    ${comment.content}
-    </p>
-    <div class="action">
-    <div class="vote-box">
-    <button>
-    <img src="images/icon-plus.svg" class="vote-button" />
-    </button>
-    <span>${comment.score}</span>
-    <button><img src="images/icon-minus.svg" /></button>
-    </div>
-    <button><img src="images/icon-reply.svg" />Reply</button>
-    </div>
-    </div>
-    <div class="replies"></div>`;
+      const commentsHtml = createHtmlComments(comment);
 
-      commentSection.insertAdjacentHTML("beforebegin", html);
+      commentSection.insertAdjacentHTML("beforebegin", commentsHtml);
 
       const replySection = document.querySelector(".replies");
 
       if (comment.replies) {
         const replies = comment.replies;
         replies.forEach((reply) => {
-          let replyHtml = `<div class="comment-box">
-          <div class="user-box">
-          <img
-          src=${reply.user.image.png}
-          alt="profile-pic-${reply.user.username}"
-          />
-          <p class="username">${comment.user.username}</p>
-          <p class="time">${reply.createdAt}</p>
-          </div>
-          <p class="text">
-          ${reply.content}
-          </p>
-          <div class="action">
-          <div class="vote-box">
-          <button>
-          <img src="images/icon-plus.svg" class="vote-button" />
-          </button>
-          <span>${comment.score}</span>
-          <button><img src="images/icon-minus.svg" /></button>
-          </div>
-          <button><img src="images/icon-reply.svg" />Reply</button>
-          </div>
-          </div>
-          <div class="replies"></div>`;
+          const replyHtml = createReplyHtml(reply);
 
           replySection.insertAdjacentHTML("afterbegin", replyHtml);
         });
